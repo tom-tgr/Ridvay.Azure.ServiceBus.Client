6cea826f7350444200727819adc1444dc2152d3c
Toms Gruzins <tomsgruzins@Gmail.com>
2023-03-25 20:28:34 +0200

    usage examples

---
+++ src/samples/Ridvay.Azure.ServiceBus.FunctionTriggerRedirect.Sample/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Ridvay.Azure.ServiceBus.Client;
+using Ridvay.Azure.ServiceBus.Client.Abstractions.FunctionTrigger;
+using Ridvay.Azure.ServiceBus.FunctionTriggerRedirect.Sample;
+using Ridvay.Azure.ServiceBus.MessageModels.Samples;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+
+namespace Ridvay.Azure.ServiceBus.FunctionTriggerRedirect.Sample;
+
+[FunctionTimerTrigger("DailyUserProfileUpdater", "0 0 * * *", typeof(BasicMessage))]
+[FunctionTimerTrigger("Every15thMinute", "*/15 * * * *", typeof(BasicMessage))]
+public class Startup : FunctionsStartup
+{
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+        var serviceBusConnectionString = Environment.GetEnvironmentVariable("ServiceBusConnection", EnvironmentVariableTarget.User);
+        builder.Services.AddServiceBusClient(serviceBusConnectionString);
+    }
+}
+
---
+++ src/samples/Ridvay.Azure.ServiceBus.MessageModels.Samples/BasicMessage.cs
@@ -0,0 +1,9 @@
+﻿using Ridvay.Azure.ServiceBus.Client.Abstractions;
+
+namespace Ridvay.Azure.ServiceBus.MessageModels.Samples;
+
+[QueueConsumer]
+public class BasicMessage
+{
+    public string SomeProperty { get; set; } = string.Empty;
+}
